27b3232300e6fa2de3be62a2699262f1addeb5cc
Sebastvin <Sebastvin@gmail.com>
2025-01-08 19:01:58 +0100

    add base server with api

---
+++ api.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type APIServer struct {
+	addr string
+}
+
+func NewAPIServer(addr string) *APIServer {
+	return &APIServer{
+		addr: addr,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := http.NewServeMux()
+	router.HandleFunc("GET /users/{userID}", func(w http.ResponseWriter, r *http.Request) {
+		userID := r.PathValue("userID")
+		w.Write([]byte("User ID: " + userID))
+	})
+
+	adminRouter := http.NewServeMux()
+	adminRouter.Handle("/api/v1/", http.StripPrefix("/api/v1", router))
+
+	middlewareChain := MiddlewareChain(
+		RequestLoggerMiddleware,
+		RequireAuthMiddleware,
+	)
+
+	server := http.Server{
+		Addr:    s.addr,
+		Handler: middlewareChain(adminRouter),
+	}
+
+	log.Printf("Server has started %s", s.addr)
+
+	return server.ListenAndServe()
+}
+
+func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("method %s, path: %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	}
+}
+
+func RequireAuthMiddleware(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+
+		if token != "Bearer token" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+type Middleware func(http.Handler) http.HandlerFunc
+
+func MiddlewareChain(middleware ...Middleware) Middleware {
+	return func(next http.Handler) http.HandlerFunc {
+		for i := len(middleware) - 1; i >= 0; i-- {
+			next = middleware[i](next)
+		}
+
+		return next.ServeHTTP
+	}
+}
